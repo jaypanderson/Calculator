fffc08b2c9634240e63507bbf6dff9fbda1f28ae
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-09-22 11:50:50 +0900

    clean up
    delete code relating to entry object focus and binding. no longer need to worry about user typing text directly into the entry objects

--- calculator.py
+++ calculator.py
@@ -269,7 +269,7 @@ root.resizable(True, True)  # Controls whether user can resize window.
 
 # add the text box for the numbers
 text_width = 400
-calculation_text = ctk.CTkEntry(root, width=text_width, takefocus="false")  # takefocus is to hide the blinking cursor.
+calculation_text = ctk.CTkEntry(root, width=text_width)
 calculation_text.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 calculation_text.configure(font=("Lucida Console", 15), state="readonly")
 calculation_text.bind("<Key>", lambda x: "break")
@@ -280,12 +280,10 @@ status_var = ctk.StringVar()
 status_var.trace_add('write', lambda *args: change_button_status(operators))
 
 # add text box for current value
-current_text = ctk.CTkEntry(root, width=text_width, insertontime=100, takefocus="false", textvariable=status_var)  # insertontime is to hide the blinking cursor.
+current_text = ctk.CTkEntry(root, width=text_width, textvariable=status_var)
 current_text.insert(0, "0")
 current_text.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
 current_text.configure(font=("Lucida Console", 30), state="readonly")
-# current_text.bind("<Key>", lambda x: "break")
-current_text.unbind("<Button-1>")
 
 # define button font and size
 button_font = ("Lucida Console", 20)
