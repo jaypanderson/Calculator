23ad6541e37189c6eb74474695c9b41996637550
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-04 14:05:47 +0900

    simple test
    add simple test to test structure of untitest

--- test.py
+++ test.py
@@ -3,11 +3,19 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
-        pass
+        global arith, calculation_text, current_text
+        arith = False
+        calculation_text.delete(0, END)
+        current_text.delete(0, END)
+        current_text.insert(0, '0')
+
 
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        global arith, calculation_text, current_text
+        button_click(1)
+        self.assertEqual(current_text.get(), '1')
 
 
 if __name__ == '__main__':
