ce00b92820891d9a8feabb66a9fe58cdbdf409f1
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-05 09:21:57 +0900

    unittest
    add test

--- test.py
+++ test.py
@@ -11,10 +11,17 @@ class MyTestCase(unittest.TestCase):
         current_text.delete(0, END)
         current_text.insert(0, '0')
 
-    def test_something(self):
+    # test number buttons
+    def test_1(self):
         global arith, calculation_text, current_text
         button_click(1)
         self.assertEqual(current_text.get(), '1')
+
+    def test_2(self):
+        global arith, calculation_text, current_text
+        arithmatic('+')
+        self.assertEqual(current_text.get(), '0')
+        self.assertEqual(calculation_text.get(), '0+')
 
 
 if __name__ == '__main__':
