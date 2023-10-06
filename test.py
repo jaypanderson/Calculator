f3cfde418912cab0542bbfcb7e64b370cedef759
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-06 09:11:24 +0900

    update test
    updated test to check multiply clicks

--- test.py
+++ test.py
@@ -17,6 +17,9 @@ class MyTestCase(unittest.TestCase):
         button_click(1)
         self.assertEqual(current_text.get(), '1')
         self.assertEqual(calculation_text.get(), '')
+        button_click(3)
+        self.assertEqual(current_text.get(), '13')
+        self.assertEqual(calculation_text.get(), '')
 
     def test_self_2(self):
         global arith, calculation_text, current_text
