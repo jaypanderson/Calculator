e6bc66acbc0f0745e7c5f302c35c8aaaff238410
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-20 23:36:45 +0900

    change import

--- calculator.py
+++ calculator.py
@@ -1,8 +1,6 @@
 # Imports
 import customtkinter as ctk
-from customtkinter import set_appearance_mode
-from tkinter import *
-from tkinter import END
+from customtkinter import set_appearance_mode, END
 # Callable is for when a function is returned as its return value
 from typing import Callable, Union
 
