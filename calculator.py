e0e106aca324df8e60a537325d093e38b711558f
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-20 23:25:23 +0900

    add init

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class SimpleCalc:
+
+    def __init__(self, root):
+        self.root = root
+        self.arith = False
+        self.init_calculator_text(self)
+        self.init.current_text(self)
+        self.init_buttons()
+
+
+if __name__ == '__main__':
+    pass
