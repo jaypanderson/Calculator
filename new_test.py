bc0e8988f86af98b82824ccaa89ea23b85928bae
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-30 09:28:43 +0900

    add helper function

--- new_test.py
+++ new_test.py
@@ -9,7 +9,10 @@ class MyTestCase(unittest.TestCase):
         # tc is short for test calc
         self.tc = SC()
 
-
-
+    def assert_state(self, exp_arith, exp_calc_text, exp_cur_text) -> None:
+        self.assertEqual(exp_arith, self.tc.arith)
+        self.assertEqual(exp_calc_text, self.tc.calculation_text.get())
+        self.assertEqual(exp_cur_text, self.tc.current_text.get())
+    
 
 
