a84244354b17c5608fee9523e659215aac1e483a
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-30 10:12:51 +0900

    fix test

--- new_test.py
+++ new_test.py
@@ -10,7 +10,6 @@ class MyTestCase(unittest.TestCase):
         # tc is short for test calc
         self.root = ctk.CTk()
         self.tc = sc(self.root)
-        #self.root.mainloop()
     def assert_state(self, exp_arith, exp_calc_text, exp_cur_text) -> None:
         self.assertEqual(exp_arith, self.tc.arith)
         self.assertEqual(exp_calc_text, self.tc.calculation_text.get())
@@ -18,7 +17,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_num(self):
         self.tc.number(1)
-        self.assert_state(False, '', '0')
+        self.assert_state(False, '', '1')
 
 if __name__ == '__main__':
     unittest.main()
