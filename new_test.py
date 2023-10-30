160dfa9198151e83c2c7fec546f737e39c37ce21
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-10-30 09:26:34 +0900

    add init

--- new_test.py
+++ new_test.py
@@ -1,2 +1,15 @@
 import unittest
-from calculator import SimpleCalc
+from calculator import SimpleCalc as SC
+
+
+
+class MyTestCase(unittest.TestCase):
+
+    def __init__(self):
+        # tc is short for test calc
+        self.tc = SC()
+
+
+
+
+
