774751a6a19b901e1610cdde0e8c5c868ed7cc05
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-11-05 19:57:32 +0900

    add test

--- new_test.py
+++ new_test.py
@@ -210,6 +210,15 @@ class MyTestCase(unittest.TestCase):
         self.tc.decimal()
         self.assert_state(False, '', '8.')  # used to appear as (True, '', '0.')
 
+    # this bug would allow endless zeros to be entered and the number at the end like this -000000004 which would
+    # confuse the evaluator and raise and error.
+    def test_founds_bugs_2(self):
+        self.tc.decimal()
+        self.assert_state(False, '', '0.')
+        self.tc.plus_minus()
+        self.assert_state(False, '', '0.')  # used to appear as (False, '', '-0.')
+
+
 
 
 if __name__ == '__main__':
