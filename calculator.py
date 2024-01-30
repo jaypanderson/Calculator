c8c6f5360cce64096893479170f095d09ce5ed90
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-01-30 17:21:58 +0900

    strip for testing

--- calculator.py
+++ calculator.py
@@ -593,52 +593,7 @@ class MatrixCalc:
         self.height = 50
         self.border = 1
 
-        # add buttons for the numbers
-        self.button_1 = ctk.CTkButton(self.root, text='1', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(1))
-        self.button_2 = ctk.CTkButton(self.root, text='2', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(2))
-        self.button_3 = ctk.CTkButton(self.root, text='3', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(3))
-        self.button_4 = ctk.CTkButton(self.root, text='4', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(4))
-        self.button_5 = ctk.CTkButton(self.root, text='5', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(5))
-        self.button_6 = ctk.CTkButton(self.root, text='6', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(6))
-        self.button_7 = ctk.CTkButton(self.root, text='7', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(7))
-        self.button_8 = ctk.CTkButton(self.root, text='8', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(8))
-        self.button_9 = ctk.CTkButton(self.root, text='9', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(9))
-        self.button_0 = ctk.CTkButton(self.root, text='0', width=self.width, height=self.height,
-                                      border_width=self.border, font=self.button_font, command=lambda: self.number(0))
-        self.button_add = ctk.CTkButton(self.root, text='+', width=self.width, height=self.height,
-                                        border_width=self.border, font=self.button_font, command=lambda: self.arithmatic('+'))
-        self.button_subtract = ctk.CTkButton(self.root, text='-', width=self.width, height=self.height,
-                                             border_width=self.border, font=self.button_font, command=lambda: self.arithmatic('-'))
-        self.button_multiply = ctk.CTkButton(self.root, text='⨉', width=self.width, height=self.height,
-                                             border_width=self.border, font=self.button_font, command=lambda: self.arithmatic('x'))
-        self.button_divide = ctk.CTkButton(self.root, text='÷', width=self.width, height=self.height,
-                                           border_width=self.border, font=self.button_font, command=lambda: self.arithmatic('÷'))
-        self.button_exponential = ctk.CTkButton(self.root, text='^', width=self.width, height=self.height,
-                                                border_width=self.border, font=self.button_font, command=lambda: self.arithmatic('^'))
-        self.button_plus_minus = ctk.CTkButton(self.root, text='+/-', width=self.width, height=self.height,
-                                               border_width=self.border, font=self.button_font, command=self.plus_minus)
-        self.button_decimal = ctk.CTkButton(self.root, text='.', width=self.width, height=self.height,
-                                            border_width=self.border, font=self.button_font, command=self.decimal)
-        self.button_backspace = ctk.CTkButton(self.root, text='<-X', width=self.width, height=self.height,
-                                              border_width=self.border, font=self.button_font, command=self.backspace)
-        self.button_clear = ctk.CTkButton(self.root, text='Clear', width=self.width, height=self.height,
-                                          border_width=self.border, font=self.button_font, command=self.clear)
-        self.button_equal = ctk.CTkButton(self.root, text='=', width=self.width * 2, height=self.height,
-                                          border_width=self.border, font=self.button_font, command=self.equal)
 
-        # TODO complete creating the different categories.
-        # organize buttons into different categories.
-        self.operators = [self.button_add, self.button_subtract, self.button_multiply, self.button_divide, self.button_exponential,
-                     self.button_decimal, self.button_plus_minus, self.button_backspace]
 
     def init_ui(self):
         set_appearance_mode('light')
@@ -650,27 +605,6 @@ class MatrixCalc:
         # get the background color of the root object
         self.root_bg_color = self.root.cget('bg')
 
-        # put the buttons on the screen
-        self.button_1.grid(row=5, column=0)
-        self.button_2.grid(row=5, column=1)
-        self.button_3.grid(row=5, column=2)
-        self.button_4.grid(row=4, column=0)
-        self.button_5.grid(row=4, column=1)
-        self.button_6.grid(row=4, column=2)
-        self.button_7.grid(row=3, column=0)
-        self.button_8.grid(row=3, column=1)
-        self.button_9.grid(row=3, column=2)
-        self.button_0.grid(row=6, column=1)
-        self.button_add.grid(row=6, column=3)
-        self.button_subtract.grid(row=5, column=3)
-        self.button_multiply.grid(row=4, column=3)
-        self.button_divide.grid(row=3, column=3)
-        self.button_exponential.grid(row=8, column=0)
-        self.button_plus_minus.grid(row=6, column=0)
-        self.button_decimal.grid(row=6, column=2)
-        self.button_clear.grid(row=7, column=1)
-        self.button_backspace.grid(row=7, column=0)
-        self.button_equal.grid(row=7, column=2, columnspan=2)
 
     def init_calculator_text(self):
         # add the text box for the numbers
@@ -684,7 +618,6 @@ class MatrixCalc:
         # Define a string variable that will be used to keep track if undefined is displayed on current_text or not
         # in order to disable certain buttons.
         self.status_var = ctk.StringVar()
-        self.status_var.trace_add('write', lambda *args: self.change_button_status(self.operators))  # not sure why *args makes this work
 
         # add text box for current value
         self.current_text = ctk.CTkEntry(self.root, width=self.text_width, textvariable=self.status_var,
@@ -696,6 +629,6 @@ class MatrixCalc:
 
 if __name__ == '__main__':
     root = ctk.CTk()
-    calc = SimpleCalc(root)
+    calc = MatrixCalc(root)
     root.mainloop()
     
