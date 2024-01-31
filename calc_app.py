d8e24e80024e7d2e2110d439e43360322be37e70
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-01-31 10:19:37 +0900

    update module documentation

--- calc_app.py
+++ calc_app.py
@@ -1,3 +1,4 @@
 """
-This will be the main script to run the calculator app.  All the 
+This will be the main script to run the calculator app.  All the different calculators will be organized in to their own
+module and class and will be imported here so that it can be run.  The default calculator will be simple calculator.
 """
