443169bceb28c0676132ea3c4da03b02eb7e4c33
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-01-31 09:15:52 +0900

    add documentation to script

---
+++ calc_app.py
@@ -0,0 +1,4 @@
+"""
+Central location for the calculator app.  The different versions of the calculator will be placed into separate modules.
+They will be imported here where the main loop for tkinter(cutomtkinter) will run.
+"""
