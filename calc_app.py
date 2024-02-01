6e50096f6d430bf8bdddf2e0cd186542a16db0f7
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-02-01 15:29:42 +0900

    update init

--- calc_app.py
+++ calc_app.py
@@ -12,7 +12,11 @@ class MainApp:
 
     def __init__(self, root):
         self.root = root
-        self.calculator_options = {'simple': SimpleCalc,
-                                   'matrix': MatrixCalc}
-        self.calculator_var = ctk.StringVar()
+        self.calculator_options = {'Simple': SimpleCalc,
+                                   'Matrix': MatrixCalc}
+        self.calculator_var = ctk.StringVar(self.root)
+        self.calculator_var.set('Simple')
+        self.option_menu = ctk.CTkOptionMenu(root, self.calculator_var, *self.calculator_options.keys(), command=self.switch_calculator)
+        self.option_menu.pack()
+        self.switch_calculator('Simple')
         pass
