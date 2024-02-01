3ca94b6c41e245c1b9b60252156db825f94849e0
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-02-01 14:22:44 +0900

    add imports

--- calc_app.py
+++ calc_app.py
@@ -2,3 +2,7 @@
 This will be the main script to run the calculator app.  All the different calculators will be organized in to their own
 module and class and will be imported here so that it can be run.  The default calculator will be simple calculator.
 """
+
+import customtkinter as ctk
+from simple_calculator import SimpleCalc
+from matrix_calculator import MatrixCalc
